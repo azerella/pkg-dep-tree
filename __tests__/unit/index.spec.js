115bfd557d55ac0cb6264e18f9fb3fcfe14df9a0
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-01-24 14:51:50 +1100

    Tweaked test, added package.json changes

--- __tests__/unit/index.spec.js
+++ __tests__/unit/index.spec.js
@@ -4,9 +4,7 @@ const Expect = require( 'chai' ).expect;
 const { ReadPkgFile } = require( '../../index.js' );
 
 describe('ReadPkgFile()', function() {
-      it('Should return valid json object given a package.json file', async function( done ) {
-        let file = await Fsp.writeFile( './package.json', {} );
-        Expect( await ReadPkgFile( './package.json' ) ).to.throw( `a` );
-        done();
+      it('Should return valid json object given a package.json file', async function( ) {
+        Expect( await ReadPkgFile( 'package.json' ) );
       });
   });
