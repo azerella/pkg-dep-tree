3101990e769e9e38cedb9abf9d9f4c6070d65a8d
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-01-25 22:47:15 +1100

    Removed dependency getters

--- index.js
+++ index.js
@@ -6,20 +6,14 @@ const Path = require( 'path' );
 const Treeify = require ( 'treeify' );
 
 
+/**
+ * Return an object with the name and version from a given package.json file
+ * 
+ * @param {object} object 
+ */
 const GetRootDependency = ( object ) => {
     let { name, version } = object;
     return { [ name ] : version }
-}
-
-
-const GetDependencies = ( object ) => {
-    return object.dependencies;
-}
-const GetDevDependencies = ( object ) => {
-    return object.devDependencies;
-}
-const GetPeerDependencies = ( object ) => {
-    return object.peerDependencies;
 }
 
 
@@ -27,8 +21,6 @@ const GetPeerDependencies = ( object ) => {
  * Asyncronously return a package.json object given a package.json file
  * 
  * @param {string} filePath     - File path location 
- * 
- * @returns {object} package.json file data as object
  */
 const ReadPkgFile = async ( filePath ) => {
     if( !filePath.includes( 'package.json' ) ) {
@@ -37,3 +29,31 @@ const ReadPkgFile = async ( filePath ) => {
     let pkgFile = await Fsp.readFile( filePath, 'utf-8' );
     return JSON.parse( pkgFile );
 }
+
+/**
+ * Return an object containing the dependency tree.
+ * 
+ * @param {string} filePath - Location of the package.json
+ */
+const GetDepTree = async ( filePath ) => {
+    let package = await ReadPkgFile( filePath );
+    let packageDeps = package.dependencies;
+    
+    let root = await GetRootDependency( package );
+    let rootName = Object.keys( root )[ 0 ]
+
+    let result = {
+        [ rootName ]: {}
+    };
+
+    Object.assign( result[ rootName ], packageDeps );
+    
+    return result;
+}
+
+
+(async () => {
+    let result = await GetDepTree( Path.join( __dirname, `package.json` ) );
+
+    console.log( Treeify.asTree( result ) );
+})();
